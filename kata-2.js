4cb194f244e263ede247bd37d8bbcfe34a3aeb0a
Danilo Gondim <daniloencarnacao@gmail.com>
2020-07-30 14:48:56 +0000

    Add a function that receives an array and adds only the numbers which match the given condition (even or odd)

---
+++ kata-2.js
@@ -0,0 +1,19 @@
+// a function that receives an array and adds only the
+// numbers which matchthe given condition (even or odd).
+
+const conditionalSum = function(values, condition) {
+  let sum = 0;
+  for (let value of values) {
+    if (condition === "even" && value % 2 === 0) {
+      sum+=value;
+    } else if (condition === "odd" && value % 2 !== 0) {
+      sum+=value;
+    }
+  }
+  return sum;
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
