a3d0b64f155a72b99ca2ee5019faaf757db8dcef
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-04 13:30:46 +0000

    Add a function that receives a url encoded string of key-value pairs, and turns it into a JavaScript object

---
+++ kata-17.js
@@ -0,0 +1,18 @@
+// function that receives a url encoded string of key-value pairs, and turns it into a JavaScript object
+
+const urlDecode = function(text) {
+  const obj = {}
+  const textArray = text.split("&");
+  for (let pair of textArray) {
+    pair = pair.replace(/%20/g, " ");
+    pair = pair.split("=");
+    obj[pair[0]] = pair[1];
+  }
+  return obj
+};
+
+console.log(urlDecode("duck=rubber"));
+console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
+console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
+
