e36373dac6fe7ef3fe90296d039b2cdbc59de239
Danilo Gondim <daniloencarnacao@gmail.com>
2020-08-04 13:33:41 +0000

    Add a function that receives a url encoded string of key-value pairs, and turns it into a JavaScript object

--- kata-17.js
+++ kata-17.js
@@ -15,4 +15,3 @@ console.log(urlDecode("duck=rubber"));
 console.log(urlDecode("bootcamp=Lighthouse%20Labs"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain"));
 console.log(urlDecode("city=Vancouver&weather=lots%20of%20rain").weather);
-
